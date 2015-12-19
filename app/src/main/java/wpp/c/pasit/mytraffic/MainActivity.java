573b475a7ba29dceb76b40be26df47556ca2858b
paisit <manote_man@hotmail.com>
2015-12-19 15:03:03 +0700

    Link YouTube finished day 1

--- app/src/main/java/wpp/c/pasit/mytraffic/MainActivity.java
+++ app/src/main/java/wpp/c/pasit/mytraffic/MainActivity.java
@@ -1,6 +1,8 @@
 package wpp.c.pasit.mytraffic;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -36,6 +38,14 @@ public class MainActivity extends AppCompatActivity {
             //Sound Effect
                 MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.effect_btn_long);
                 buttonMediaPlayer.start();
+
+
+                //Intent To WebView
+                Intent objIntent = new Intent(new Intent(Intent.ACTION_VIEW));
+                objIntent.setData(Uri.parse("https://youtu.be/ABj9jFzwN24?list=PL795814BC3374595A"));
+                startActivity(objIntent);
+
+                //
 
             } // event
         });
