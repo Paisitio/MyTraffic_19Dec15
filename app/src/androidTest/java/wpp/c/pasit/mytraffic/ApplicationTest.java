0fa9d706e7f5b6a00df4362070cedfc10fb54620
paisit <manote_man@hotmail.com>
2015-12-19 11:40:42 +0700

    Start

---
+++ app/src/androidTest/java/wpp/c/pasit/mytraffic/ApplicationTest.java
@@ -0,0 +1,13 @@
+package wpp.c.pasit.mytraffic;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
