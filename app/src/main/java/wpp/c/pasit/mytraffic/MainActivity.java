99c54f9f99a7202f5653296f887b055b250231c1
paisit <manote_man@hotmail.com>
2015-12-19 14:47:21 +0700

    ออกแบบsoundเสร็จแล้ว

--- app/src/main/java/wpp/c/pasit/mytraffic/MainActivity.java
+++ app/src/main/java/wpp/c/pasit/mytraffic/MainActivity.java
@@ -1,13 +1,48 @@
 package wpp.c.pasit.mytraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        //Button Controller
+        buttonController();
+
+
+    } // Main Method
+
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            //Sound Effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.effect_btn_long);
+                buttonMediaPlayer.start();
+
+            } // event
+        });
     }
-}
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+    }
+} // Main Class
